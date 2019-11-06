5ffbde704ac6944b1599eff3baf37242fd918162
KaterynaSimon <kateryna.simon@nure.ua>
2019-11-06 20:23:52 +0200

    Creating a test method

---
+++ src/test/java/nure/kn/simon/domain_db/HsqldbUserDaoTest.java
@@ -0,0 +1,58 @@
+package nure.kn.simon.domain_db;
+
+import java.util.Calendar;
+
+import nure.kn.simon.domain.User;
+
+import nure.kn.simon.domain_db.HsqldbUserDao;
+
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+
+public class HsqldbUserDaoTest extends DatabaseTestCase {
+
+	private HsqldbUserDao dao;
+	private static final String LAST_NAME = "Simon";
+	private static final String FIRST_NAME = "Kate";
+	private static final int DAY_OF_BIRTH = 13;
+	private static final int MONTH = 12;
+	private static final int YEAR = 1999;
+
+	public void testCreate() throws DatabaseException{
+		User user = new User();
+		user.setFirstName(FIRST_NAME);
+		user.setLastName(LAST_NAME);
+		Calendar calendar = Calendar.getInstance(); 
+		calendar.set(YEAR, MONTH, DAY_OF_BIRTH);
+		user.setDateOfBirth(calendar.getTime());
+		assertNull(user.getId());
+		User userToCheck = dao.create(user);
+		assertNotNull(userToCheck);
+		assertNotNull(userToCheck.getId());
+		assertEquals(FIRST_NAME, userToCheck);
+		assertEquals(LAST_NAME, userToCheck);
+		assertEquals(calendar.getTime(), userToCheck.getDateOfBirth());
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Override
+	protected IDatabaseConnection getConnection() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
